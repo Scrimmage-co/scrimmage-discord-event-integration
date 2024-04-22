5cd57a6ad6b855c0e09d5bc67ca5225b55bdd52f
yevgeniusr <yev.rachkovan@gmail.com>
2024-04-22 22:27:17 +0700

    Fix env check

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Configuration } from './configurations';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<Configuration>);
-  const logger = app.get(Logger);
   const requiredEnvs = [
     'DISCORD_TOKEN',
     'SCRIMMAGE_API_SERVER_ENDPOINT',
@@ -18,13 +16,10 @@ async function bootstrap() {
   ];
   for (const env of requiredEnvs) {
     if (!configService.get(env as any)) {
-      logger.error(`Missing required environment variable: ${env}`);
+      console.error(`Missing required environment variable: ${env}`);
       process.exit(1);
     }
   }
-  logger.log(
-      `Starting server on ${configService.get('HOSTNAME')}:${configService.get('PORT')}`,
-  );
   await app.listen(configService.get('PORT'), configService.get('HOSTNAME'));
   app.enableShutdownHooks();
 }
